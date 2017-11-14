495d99c65ef26429882dcdfed092a1770cea114b
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2017-11-14 14:48:19 +0100

    test the tree structure

--- com.examples.ui.tests/src/com/examples/ui/tests/SampleViewTest.java
+++ com.examples.ui.tests/src/com/examples/ui/tests/SampleViewTest.java
@@ -3,6 +3,7 @@ package com.examples.ui.tests;
 import static org.eclipse.swtbot.swt.finder.waits.Conditions.shellCloses;
 
 import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
+import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
 import org.junit.BeforeClass;
@@ -28,8 +29,12 @@ public class SampleViewTest {
 	}
 
 	@Test
-	public void test() {
-
+	public void testViewTree() {
+		SWTBotView view = bot.viewByTitle("Sample View");
+		view.bot().tree().getTreeItem("Root").expand();
+		view.bot().tree().getTreeItem("Root").getNode("Parent 1").expand();
+		view.bot().tree().getTreeItem("Root").getNode("Parent 1").
+			getNode("Leaf 1").select();
 	}
 
 }
