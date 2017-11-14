533c5ac0024061225a6cc9f7eea4e5fe9704ab41
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2017-11-14 16:34:48 +0100

    added UI structure to CalculatorView

--- com.examples.view/src/com/examples/view/CalculatorView.java
+++ com.examples.view/src/com/examples/view/CalculatorView.java
@@ -2,22 +2,45 @@ package com.examples.view;
 
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Button;
 
 public class CalculatorView extends ViewPart {
+	private Text inputText;
+	private Text outputText;
 
 	public CalculatorView() {
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void createPartControl(Composite parent) {
-		// TODO Auto-generated method stub
-
+		parent.setLayout(new GridLayout(3, false));
+		
+		Label lblNewLabel = new Label(parent, SWT.NONE);
+		lblNewLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		lblNewLabel.setText("Input");
+		
+		inputText = new Text(parent, SWT.BORDER);
+		inputText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		
+		Button btnDouble = new Button(parent, SWT.NONE);
+		btnDouble.setText("Double");
+		
+		Label lblNewLabel_1 = new Label(parent, SWT.NONE);
+		lblNewLabel_1.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		lblNewLabel_1.setText("Output");
+		
+		outputText = new Text(parent, SWT.BORDER);
+		outputText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		new Label(parent, SWT.NONE);
 	}
 
 	@Override
 	public void setFocus() {
-		// TODO Auto-generated method stub
 
 	}
 
