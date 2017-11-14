959e31f7bb78116ee0441eb8e1a85cfe4e1f73fc
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2017-11-14 13:48:39 +0100

    added other tests for the menu

--- com.examples.ui.tests/src/com/examples/ui/tests/SampleMenuTest.java
+++ com.examples.ui.tests/src/com/examples/ui/tests/SampleMenuTest.java
@@ -2,8 +2,12 @@ package com.examples.ui.tests;
 
 import static org.eclipse.swtbot.swt.finder.waits.Conditions.shellCloses;
 
+import org.eclipse.jface.bindings.keys.KeyStroke;
+import org.eclipse.jface.bindings.keys.ParseException;
 import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
+import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
+import org.eclipse.swtbot.swt.finder.keyboard.Keystrokes;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -18,6 +22,17 @@ public class SampleMenuTest {
 	@BeforeClass
 	public static void initBot() {
 		bot = new SWTWorkbenchBot();
+		// Welcome view prevents for reaching the toolbar
+		// so we need to close it
+		closeWelcomePage();
+	}
+
+	private static void closeWelcomePage() {
+		for (SWTBotView view : bot.views()) {
+			if (view.getTitle().equals("Welcome")) {
+				view.close();
+			}
+		}
 	}
 
 	@AfterClass
@@ -28,6 +43,22 @@ public class SampleMenuTest {
 	@Test
 	public void testSampleMenu() {
 		bot.menu("Sample Menu").menu("Sample Command").click();
+		assertDialog();
+	}
+
+	@Test
+	public void testToolbar() {
+		bot.toolbarButtonWithTooltip("Say hello world (Ctrl+6)").click();
+		assertDialog();
+	}
+
+	@Test
+	public void testShortcut() throws ParseException {
+		bot.activeShell().pressShortcut(Keystrokes.MOD1, KeyStroke.getInstance("6"));
+		assertDialog();
+	}
+
+	private void assertDialog() {
 		SWTBotShell dialog = bot.shell("Menu");
 		dialog.activate();
 		bot.label("Hello, Eclipse world");
