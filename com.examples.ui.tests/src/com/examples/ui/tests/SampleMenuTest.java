8a0cba6bc1b7565058fad59fa85bdc2b8ae8bc86
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2017-11-15 08:52:36 +0100

    modified SampleMenuTest

--- com.examples.ui.tests/src/com/examples/ui/tests/SampleMenuTest.java
+++ com.examples.ui.tests/src/com/examples/ui/tests/SampleMenuTest.java
@@ -4,41 +4,11 @@ import static org.eclipse.swtbot.swt.finder.waits.Conditions.shellCloses;
 
 import org.eclipse.jface.bindings.keys.KeyStroke;
 import org.eclipse.jface.bindings.keys.ParseException;
-import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
-import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
-import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
 import org.eclipse.swtbot.swt.finder.keyboard.Keystrokes;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
-@RunWith(SWTBotJunit4ClassRunner.class)
-public class SampleMenuTest {
-
-	private static SWTWorkbenchBot bot;
-
-	@BeforeClass
-	public static void initBot() {
-		bot = new SWTWorkbenchBot();
-		// Welcome view prevents for reaching the toolbar
-		// so we need to close it
-		closeWelcomePage();
-	}
-
-	private static void closeWelcomePage() {
-		for (SWTBotView view : bot.views()) {
-			if (view.getTitle().equals("Welcome")) {
-				view.close();
-			}
-		}
-	}
-
-	@AfterClass
-	public static void afterClass() {
-		bot.resetWorkbench();
-	}
+public class SampleMenuTest extends AbstractTest {
 
 	@Test
 	public void testSampleMenu() {
